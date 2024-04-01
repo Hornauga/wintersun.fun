a5339cf282755e94c219f07b8ad1b7a1a7e28457
Ragnar English <RagnarArdal@gmail.com>
2024-04-01 17:21:18 +0100

    Make the project a TypeScript one (#6)
    * Install typescript modules
    
    * Make the config a TypeScript one
    
    * Make vite config a TypeScript file
    
    * Delete unneeded files and make the others ts ones
    
    * Remove test command
    
    * Configure as Vite recommended
    
    * Add packages and make linting script
    
    * Refer to TS index
    
    * Clean up index.tsx
    
    * Add prettier as well
    
    * Run prettier

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="icons/wintersun.png"
+          className="App-logo"
+          alt="Wintersun logo"
+        />
+        <p>Spreading the Wintersun fun!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
