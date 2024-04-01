c4b9eb4d154b98df794c877bcfa9d7bb07e42e86
Ragnar English <RagnarArdal@gmail.com>
2024-04-01 16:16:01 +0000

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="icons/wintersun.png" className="App-logo" alt="Wintersun logo" />
-        <p>
-          Spreading the Wintersun fun!
-        </p>
+        <img
+          src="icons/wintersun.png"
+          className="App-logo"
+          alt="Wintersun logo"
+        />
+        <p>Spreading the Wintersun fun!</p>
       </header>
     </div>
   );
