40cb479e42679b6a2c7e649b0d40c9cb318ffe1c
Ragnar English <RagnarArdal@gmail.com>
2024-04-01 19:37:41 +0100

    Add Wintersun album (#7)
    * Include tsx in tsconfig include
    
    * Use 'title' instead of 'name'
    
    * Display example using music source

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
 import "./App.css";
+import { Song, allSongs } from "./music.ts";
+
+function YouTubeEmbed({ song }: { song: Song }) {
+  const src = `https://www.youtube.com/embed/${song.src.youtube}`;
+  console.log(src);
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={src}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+  );
+}
 
 function App() {
   return (
@@ -11,6 +28,21 @@ function App() {
         />
         <p>Spreading the Wintersun fun!</p>
       </header>
+      {allSongs.map((song) => (
+        <div>
+          <h3>{song.title}</h3>
+          <p>
+            <YouTubeEmbed song={song} />
+          </p>
+          {"spotify" in song.src && (
+            <p>
+              <a href={`https://open.spotify.com/track/${song.src.spotify}`}>
+                Listen on Spotify
+              </a>
+            </p>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/music.ts
@@ -0,0 +1,75 @@
+export interface Album {
+  title: string;
+  artist: string;
+  src: {
+    youtube?: string;
+    spotify?: string;
+  };
+}
+
+export interface Song {
+  title: string;
+  album?: Album;
+  src: {
+    youtube?: string;
+    spotify?: string;
+  };
+}
+
+export const albumWintersunWintersun: Album = {
+  title: "Wintersun",
+  artist: "Wintersun",
+  src: { youtube: "XfMrC0dvvmw" },
+};
+export const albumWintersunWintersunSongs: Song[] = [
+  {
+    title: "Winter Madness Intro",
+    album: albumWintersunWintersun,
+    src: { youtube: "GUXpbnT5eBE" },
+  },
+  {
+    title: "Winter Madness",
+    album: albumWintersunWintersun,
+    src: {
+      youtube: "GUXpbnT5eBE?start=155",
+      spotify: "2N1SFSnHMIdHY0waUbjEnv",
+    },
+  },
+  {
+    title: "Beyond The Dark Sun",
+    album: albumWintersunWintersun,
+    src: { youtube: "KuG6lJ6xbKk", spotify: "1L3S5vFgsVzweUJnAIfSGM" },
+  },
+  {
+    title: "Sleeping Stars",
+    album: albumWintersunWintersun,
+    src: { youtube: "hDsTtJXdMZo", spotify: "5MqVSSinDohOuMjNhO6PU6" },
+  },
+  {
+    title: "Battle Against Time",
+    album: albumWintersunWintersun,
+    src: { youtube: "b4E0qN4_xQI", spotify: "0tEA8vx1V2mJoxSIqapi0H" },
+  },
+  {
+    title: "Death and the Healing",
+    album: albumWintersunWintersun,
+    src: { youtube: "wX-X7oX5Abc", spotify: "6EAsLKCDMQV3BRRb9OdaMJ" },
+  },
+  {
+    title: "Starchild",
+    album: albumWintersunWintersun,
+    src: { youtube: "oyuOjCfCsm4", spotify: "3LPVfzrc9G8EOQ00nTgvKL" },
+  },
+  {
+    title: "Beautiful Death",
+    album: albumWintersunWintersun,
+    src: { youtube: "TNg-mfVnCWk", spotify: "6S83nQwA0aPHKB86vAqgl5" },
+  },
+  {
+    title: "Sadness and Hate",
+    album: albumWintersunWintersun,
+    src: { youtube: "x3s19wDvyeU", spotify: "4cN0IBNbFkq0srzoyNOC4C" },
+  },
+];
+
+export const allSongs: Song[] = [...albumWintersunWintersunSongs];
