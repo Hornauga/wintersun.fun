bed220826b62c4861eef08070d41087c5dda7722
Ragnar English <RagnarArdal@gmail.com>
2024-04-04 18:33:51 +0100

    Add basic navigation bar (#10)
    * Rename app.tsx so files are consistent
    
    * Modularize a bit
    
    * Put background logo in center where it belongs
    
    * Change colors, add responsive fonts, move container
    
    * WIP pages because codespaces is bugging out
    
    * Apply navigation bar to all pages
    
    * Remove navbar file
    
    * Fix error with TODOs

---
+++ src/app.tsx
@@ -0,0 +1,40 @@
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  responsiveFontSizes,
+} from "@mui/material";
+import { useState } from "react";
+import { blue, indigo } from "@mui/material/colors";
+import { Page, PageName } from "./pages";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const theme = responsiveFontSizes(
+  createTheme({
+    // https://mui.com/material-ui/customization/theming/
+    // https://mui.com/material-ui/customization/color/
+    palette: {
+      mode: "dark",
+      primary: indigo,
+      secondary: blue,
+    },
+  }),
+);
+
+function App() {
+  const [pageName, setPageName] = useState<PageName>("welcome");
+  function navigate(pageName: PageName) {
+    setPageName(pageName);
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Page pageName={pageName} navigate={navigate}></Page>
+    </ThemeProvider>
+  );
+}
+
+export default App;
