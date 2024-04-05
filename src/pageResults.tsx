1069c93e74abfeaf50998f8358b079cf1276edac
Ragnar English <RagnarArdal@gmail.com>
2024-04-05 17:52:03 +0000

    Refactor embed components out and add lipsum

--- src/pageResults.tsx
+++ src/pageResults.tsx
@@ -1,5 +1,8 @@
 import { Typography, Box } from "@mui/material";
-import { Song, allSongs } from "./music.ts";
+import { Song, allSongs } from "./music";
+import { YouTubeEmbed, SpotifyEmbed } from "./music";
+
+import { loremIpsum } from "./misc";
 
 export function ResultsPage() {
   return (
@@ -11,41 +14,6 @@ export function ResultsPage() {
   );
 }
 
-function YouTubeEmbed({ song }: { song: Song }) {
-  const src = `https://www.youtube.com/embed/${song.src.youtube}`;
-  return (
-    <iframe
-      src={src}
-      width="100%"
-      height="315px"
-      style={{ border: 0 }}
-      allow="fullscreen; autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-    ></iframe>
-  );
-}
-
-function SpotifyEmbed({ song }: { song: Song }) {
-  const src = `https://open.spotify.com/embed/track/${song.src.spotify}`;
-  // https://stackoverflow.com/q/1234127
-  // https://stackoverflow.com/a/76903187
-  // You're not supposed to style the background, you're supposed to use one of the supported heights and then the player has it's own colour for the embed.
-  // In your case, do all of these:
-  // Change the height to 80 (152 and 352 are larger options).
-  // Add frameBorder="0" to get rid of the surrounding white lines.
-  // Add style="border-radius:12px" to get rid of the white corners.
-  return (
-    <iframe
-      src={src}
-      width="100%"
-      height="80px"
-      allow="fullscreen; autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      style={{
-        border: 0,
-        borderRadius: 13,
-      }}
-    ></iframe>
-  );
-}
 function SongDisplay({ song }: { song: Song }) {
   return (
     <div>
@@ -57,8 +25,9 @@ function SongDisplay({ song }: { song: Song }) {
         <YouTubeEmbed song={song} />
       </p>
       <p>{"spotify" in song.src && <SpotifyEmbed song={song} />}</p>
-      <Typography variant="body1">
-        Here's why you'll like this song: it's black melodic death power metal
+      <Typography variant="body1" align="justify">
+        Here's why you'll like this song: it's black melodic death power metal.{" "}
+        {loremIpsum}
       </Typography>
       {"spotify" in song.src && (
         <p>
