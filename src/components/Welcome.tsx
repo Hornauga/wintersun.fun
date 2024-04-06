5848701d6a18ce7a4bbc7191c4727b57da4a5fe0
Ragnar English <RagnarArdal@gmail.com>
2024-04-06 11:06:02 +0000

    Make TL;DR blurb

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,6 +5,20 @@ export function WelcomePage() {
   return (
     <Box>
       <Typography variant="h4" align="center">
+        TL;DR
+      </Typography>
+      <Typography variant="body1" align="justify">
+        <a href="https://en.wikipedia.org/wiki/Wintersun">Wintersun</a> is how{" "}
+        <a href="https://en.wikipedia.org/wiki/Jari_M%C3%A4enp%C3%A4%C3%A4">
+          Jari Mäenpää
+        </a>{" "}
+        makes music today. The purpose of this website is to bridge the gap
+        between you and Wintersun's brilliance, because more listeners means
+        more Wintersun music! Press <strong>FILTERS</strong> (top right) to
+        select your musical preferences and receive a bespoke introduction to
+        all the music Jari has been involved in making.
+      </Typography>
+      <Typography variant="h4" align="center">
         Hello
       </Typography>
       <Typography variant="body1" align="justify">
@@ -15,6 +29,7 @@ export function WelcomePage() {
       </Typography>
       <Typography variant="body1" align="justify">
         Wintersun is Jari, and Jari so and so.
+        <a href="https://en.wikipedia.org/wiki/Ensiferum">Ensiferum</a>
         {loremIpsum}
       </Typography>
       <Typography variant="h4" align="center">
@@ -22,6 +37,9 @@ export function WelcomePage() {
       </Typography>
       <Typography variant="body1" align="justify">
         {loremIpsum}
+        My motivation for making this website is altruistic because you are
+        missing out if you are not listening to Wintersun, and the more people
+        listen to Wintersun, the more likely we are to see more Wintersun music.
       </Typography>
     </Box>
   );
