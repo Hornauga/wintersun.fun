fc690d159ebd98c48be709cfc0b6339da749b98f
Ragnar English <RagnarArdal@gmail.com>
2024-04-08 18:05:27 +0000

    Add instrumental TFS

---
+++ src/music/albumTheForestSeasonsInstrumental.tsx
@@ -0,0 +1,100 @@
+import { Album, Song } from ".";
+
+export const albumTheForestSeasonsInstrumental: Album = {
+  title: "The Forest Seasons",
+  artist: "Wintersun",
+  src: { youtube: "ZFfN3G2vjqM", spotify: "7ql5kbq4iJKLT2lpiaiZSr" },
+};
+
+export const songSpringInstrumental: Song = {
+  album: albumTheForestSeasonsInstrumental,
+  title: "Awaken from the Dark Slumber (Spring) (Instrumental)",
+  src: { youtube: "wbq72xgsdwo", spotify: "479BAqWRmIPnzXWp4cbeH4" },
+  qualityValues: {
+    // Done (copy of non-instrumental with vocals 0)
+    jari: 0,
+    black: 3,
+    death: 0,
+    folk: 2,
+    power: 1,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 1,
+    riffs: 2,
+    solos: 0,
+    energetic: 2,
+    epic: 2,
+    intense: 2,
+    melancholic: 2,
+  },
+};
+
+export const songSummerInstrumental: Song = {
+  album: albumTheForestSeasonsInstrumental,
+  title: "The Forest That Weeps (Summer) (Instrumental)",
+  src: { youtube: "kAZpX88stks", spotify: "4bqmtsRT4aU0vekZTspAUq" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songAutumnInstrumental: Song = {
+  album: albumTheForestSeasonsInstrumental,
+  title: "Eternall Darkness (Autumn) (Instrumental)",
+  src: { youtube: "G6oRbo2YDXM", spotify: "3HBFFlGOxz4eSQ719VBarS" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songWinterInstrumental: Song = {
+  album: albumTheForestSeasonsInstrumental,
+  title: "Loneliness (Winter) (Instrumental)",
+  src: { youtube: "z34CxDbywFE", spotify: "1MWR66elh7xBvrONTuqoun" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
