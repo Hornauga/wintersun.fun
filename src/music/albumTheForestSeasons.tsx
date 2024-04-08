1927fae31c8f053daab8acb0505319ea3347a92b
Ragnar English <RagnarArdal@gmail.com>
2024-04-08 17:50:02 +0000

    Add The Forest Seasons and one ranking

---
+++ src/music/albumTheForestSeasons.tsx
@@ -0,0 +1,100 @@
+import { Album, Song } from ".";
+
+export const albumTheForestSeasons: Album = {
+  title: "The Forest Seasons",
+  artist: "Wintersun",
+  src: { youtube: "ZFfN3G2vjqM", spotify: "7ql5kbq4iJKLT2lpiaiZSr" },
+};
+
+export const songSpring: Song = {
+  album: albumTheForestSeasons,
+  title: "Awaken from the Dark Slumber (Spring)",
+  src: { youtube: "9pQvOyo1nfQ", spotify: "46f36Ds9aJhDhbQsZajbih" },
+  qualityValues: {
+    // Done
+    jari: 0,
+    black: 3,
+    death: 0,
+    folk: 2,
+    power: 1,
+    thrash: 0,
+    clean: 1,
+    extreme: 3,
+    melodic: 1,
+    riffs: 2,
+    solos: 0,
+    energetic: 2,
+    epic: 2,
+    intense: 2,
+    melancholic: 2,
+  },
+};
+
+export const songSummer: Song = {
+  album: albumTheForestSeasons,
+  title: "The Forest That Weeps (Summer)",
+  src: { youtube: "ffQ2B5qegRg", spotify: "22mbGl4jUbr7bhqWOzejjt" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songAutumn: Song = {
+  album: albumTheForestSeasons,
+  title: "Eternall Darkness (Autumn)",
+  src: { youtube: "cwB3BQ0zWOw", spotify: "6NVJrvr7jKmdV04xhGG4TH" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songWinter: Song = {
+  album: albumTheForestSeasons,
+  title: "Loneliness (Winter)",
+  src: { youtube: "qIiZFpOQnd8", spotify: "6Qb4UHym7uBDGHRU0LYNjk" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
