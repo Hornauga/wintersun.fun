7e978dcdc8494538c269119da89a2b3b6128ddbb
Ragnar English <RagnarArdal@gmail.com>
2024-04-08 18:21:33 +0100

    Add Time I sources

---
+++ src/music/albumTimeI.tsx
@@ -0,0 +1,122 @@
+import { Album, Song } from ".";
+
+export const albumWintersunTimeI: Album = {
+  title: "Time I",
+  artist: "Wintersun",
+  src: { spotify: "0V32xSGyy0TyOzbc5ufoWX" },
+};
+
+export const songWhenTimeFadesAway: Song = {
+  album: albumWintersunTimeI,
+  title: "When Time Fades Away",
+  src: { youtube: "mzEv-4xcYig", spotify: "3e4hga1m26s0WqeIodSIH3" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songSonsOfWinterAndStars: Song = {
+  album: albumWintersunTimeI,
+  title: "Sons of Winter and Stars",
+  src: { youtube: "6RJnBUbAeMk", spotify: "6EJfuLTl01JoGXt8rtM1rd" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songLandOfSnowAndSorrow: Song = {
+  album: albumWintersunTimeI,
+  title: "Land of Snow and Sorrow",
+  src: { youtube: "4cdT5u7-BV0", spotify: "3WpmO13nUtLDRZujMgu4ss" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songDarknessAndFrost: Song = {
+  album: albumWintersunTimeI,
+  title: "Darkness and Frost",
+  src: { youtube: "SXwku4hxqhY", spotify: "0QvkDPezujrAC8PhnAoAQY" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songTime: Song = {
+  album: albumWintersunTimeI,
+  title: "Time",
+  src: { youtube: "SXwku4hxqhY?start=147", spotify: "2Egr3daQL5BbHHN1z75ArW" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
