43fae95f05765e20deaff309b530995ec837cdbe
Ragnar English <RagnarArdal@gmail.com>
2024-04-10 07:47:04 +0000

    Time 1 Instrumental (very unofficial)

---
+++ src/music/albumTimeIInstrumental.tsx
@@ -0,0 +1,121 @@
+import { Album, Song } from ".";
+
+export const albumWintersunTimeIInstrumental: Album = {
+  title: "Time I (Instrumental)",
+  artist: "Wintersun",
+};
+
+export const songWhenTimeFadesAway: Song = {
+  album: albumWintersunTimeIInstrumental,
+  title: "When Time Fades Away",
+  src: { youtube: "1Sc1LIYlzuQ" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songSonsOfWinterAndStars: Song = {
+  album: albumWintersunTimeIInstrumental,
+  title: "Sons of Winter and Stars (Instrumental)",
+  src: { youtube: "1Sc1LIYlzuQ&start=257" },
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songLandOfSnowAndSorrow: Song = {
+  album: albumWintersunTimeIInstrumental,
+  title: "Land of Snow and Sorrow (Instrumental)",
+  src: { youtube: "k1WqOsBRTwo"},
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songDarknessAndFrost: Song = {
+  album: albumWintersunTimeIInstrumental,
+  title: "Darkness and Frost",
+  src: { youtube: "68B3fhunbsU"},
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const songTime: Song = {
+  album: albumWintersunTimeIInstrumental,
+  title: "Time",
+  src: { youtube: "68B3fhunbsU&start=147"},
+  qualityValues: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
