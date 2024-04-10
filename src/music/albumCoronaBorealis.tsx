105c6827605ae9d65f7642572035baf8742c28ed
Ragnar English <RagnarArdal@gmail.com>
2024-04-10 09:32:11 +0000

    Add considerations for another time

---
+++ src/music/albumCoronaBorealis.tsx
@@ -0,0 +1,5 @@
+// https://en.wikipedia.org/wiki/Cadacross
+// Jari wrote and performed (?) the solo for Bring Out Your Dead
+// Could consider it
+// Year: 2002
+// https://open.spotify.com/track/3VH2u3AdHyOLf0OoqbSUFD
---
+++ src/music/albumDevsIratvs.tsx
@@ -0,0 +1,3 @@
+// We could consider including Devs - Iratvs by Arthemesia
+// https://www.youtube.com/watch?v=ZAzgXBcN4Ng
+// Year: 2001
