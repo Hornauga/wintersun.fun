a800b343d429a05207f46d1a3a52e24fb4126f54
Ragnar English <RagnarArdal@gmail.com>
2024-04-10 17:05:57 +0000

    Add latest releases

---
+++ src/music/sources/wintersun/misc.tsx
@@ -0,0 +1,90 @@
+import { Album, Song } from "../../things";
+import { Wintersun } from "../../artists";
+
+export const songWarning: Song = {
+  title: "Warning",
+  release: { artist: Wintersun, year: 2023, src: {} },
+  src: { youtube: "HxSHqSK-EFE" },
+  qualities: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    singalong: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const albumSongsOfJariIII: Album = {
+  title: "The Songs of Jari Mäenpää III",
+  artist: Wintersun,
+  year: 2024,
+  wiki: new URL("https://www.indiegogo.com/projects/wintersun-time-ii-pre-order-with-time-package"),
+  src: { },
+};
+
+export const songRedHorizon: Song = {
+  title: "Red Horizon (The Pirate Song)",
+  release: albumSongsOfJariIII,
+  src: { youtube: "hYKIONR-abc" },
+  qualities: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    singalong: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
+
+export const albumFantasyMetalProject: Album = {
+  title: "Fantasy Metal Project",
+  artist: Wintersun,
+  year: 2024,
+  wiki: new URL("https://www.indiegogo.com/projects/wintersun-time-ii-pre-order-with-time-package"),
+  src: { },
+};
+
+export const songSteelOfTheGods: Song = {
+  title: "Steel of the Gods",
+  release: albumFantasyMetalProject,
+  src: { youtube: "4x36OoFYGyE" },
+  qualities: {
+    jari: 0,
+    black: 0,
+    death: 0,
+    folk: 0,
+    power: 0,
+    thrash: 0,
+    clean: 0,
+    extreme: 0,
+    singalong: 0,
+    melodic: 0,
+    riffs: 0,
+    solos: 0,
+    energetic: 0,
+    epic: 0,
+    intense: 0,
+    melancholic: 0,
+  },
+};
