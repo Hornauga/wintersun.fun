849c0dd6e77683a2068709339d351c8f52140d72
Ragnar English <RagnarArdal@gmail.com>
2024-04-13 10:34:29 +0000

    Use Google CDN fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,10 +13,6 @@ import {
   responsiveFontSizes,
 } from "@mui/material";
 import { blue, indigo } from "@mui/material/colors";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 import Welcome from "./Welcome.tsx";
 import Filters from "./Filters.tsx";
