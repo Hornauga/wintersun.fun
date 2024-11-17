2efd5afc5c9bb2f417a92fd1427670c86386d9ca
Ragnar English <RagnarArdal@gmail.com>
2024-11-17 15:08:48 +0000

    Fix formatting errors in Time II recommendation article

--- src/components/articles/TimeIIRecommendation.tsx
+++ src/components/articles/TimeIIRecommendation.tsx
@@ -8,12 +8,11 @@ export default function TimeIIRecommendation({ song }: { song: Song }) {
   return (
     <>
       <Typography variant="body1" align="justify">
-        If you enjoy this song, perhaps you'll really like {song.title}
-        from{" "}
+        If you enjoy this song, perhaps you'll really like {song.title} from{" "}
         <a href={Wintersun.wiki.toString()} target="_blank">
           {Wintersun.title}
         </a>
-        's most recent album,
+        's most recent album,{" "}
         <a href={albumTimeII.wiki.toString()} target="_blank">
           {albumTimeII.title}
         </a>
